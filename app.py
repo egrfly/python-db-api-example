5e8769d069a7511cf7f11b118201a74016c5229e
egrfly <egrfly@outlook.com>
2023-03-30 18:23:37 +0100

    Add complete application

--- app.py
+++ app.py
@@ -6,22 +6,32 @@ app = Flask(__name__)
 
 @app.get('/bookings')
 def view_all_bookings():
-    pass
+    return jsonify(db.get_bookings())
 
 
 @app.post('/bookings')
 def make_booking():
-    pass
+    booking_data = request.get_json()
+    db.create_booking(booking_data.get('guest_name'),
+                      booking_data.get('arrival_date'),
+                      booking_data.get('departure_date'))
+    return Response(status=204)
 
 
 @app.put('/bookings/<int:booking_id>')
 def modify_booking(booking_id):
-    pass
+    new_booking_data = request.get_json()
+    db.update_booking(booking_id,
+                      new_booking_data.get('guest_name'),
+                      new_booking_data.get('arrival_date'),
+                      new_booking_data.get('departure_date'))
+    return Response(status=204)
 
 
 @app.delete('/bookings/<int:booking_id>')
 def remove_booking(booking_id):
-    pass
+    db.delete_booking(booking_id)
+    return Response(status=204)
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -11,16 +11,32 @@ def get_db_connection():
 
 
 def get_bookings():
-    pass
+    with get_db_connection() as connection:
+        with connection.cursor(dictionary=True) as cursor:
+            cursor.execute("""SELECT * FROM bookings""")
+            return cursor.fetchall()
 
 
 def create_booking(guest_name, arrival_date, departure_date):
-    pass
+    with get_db_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("""INSERT INTO bookings (guest_name, arrival_date, departure_date)
+                              VALUES (%s, %s, %s)""", [guest_name, arrival_date, departure_date])
+        connection.commit()
 
 
 def update_booking(booking_id, guest_name, arrival_date, departure_date):
-    pass
+    with get_db_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("""UPDATE bookings
+                              SET guest_name = %s, arrival_date = %s, departure_date = %s
+                              WHERE id = %s""", [guest_name, arrival_date, departure_date, booking_id])
+        connection.commit()
 
 
 def delete_booking(booking_id):
-    pass
+    with get_db_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("""DELETE FROM bookings
+                              WHERE id = %s""", [booking_id])
+        connection.commit()
